34bf8f60df20cabf75ef51d764ac0d44070193ca
Jacob Davis <ismyhc@gmail.com>
2022-07-27 10:19:29 -0400

    Initial Commit

---
+++ BOW/BOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  BOWApp.swift
+//  BOW
+//
+//  Created by Jacob Davis on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct BOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
